b13baf5baae57a455eec9efeecf338c005b70433
Beste <bestegursoy52@hotmail.com>
2025-01-02 22:25:56 +0300

    backend api ile front entegre başlandı

--- frontend/js/create.js
+++ frontend/js/create.js
@@ -44,3 +44,32 @@ document.getElementById('createbtn').addEventListener('click', function(){
         alert('Welcome! ' + username);
     }
 });
+
+/* document.getElementById("loginForm").addEventListener("submit", async function (event) {
+    event.preventDefault(); // Sayfanın yenilenmesini önler
+  
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch("http://localhost:8000/auth_service/signin/",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        });
+        
+        const data = await response.json();
+        document.getElementById("responseMessage").textContent = data.message;
+
+        if (data.message === "succes"){
+            alert("Başarılı Giriş");
+            //kullanıcıyı baska bir sayfaya gönder
+            //window.location.href = "/login.html";
+        }
+    }catch (error){
+        console.error("Hata:", error);
+        document.getElementById("responseMessage").textContent = "Bir hata oluştu.";
+    }
+}); */
